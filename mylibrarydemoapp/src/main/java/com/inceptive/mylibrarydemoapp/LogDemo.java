d1ebb525764b2965db86fc422d10710cf72219bb
rohitpanhalkar <rohit.panhalkar@inceptiveconsulting.com>
2017-05-03 12:59:56 +0530

    library for addition of two numbers

--- mylibrarydemoapp/src/main/java/com/inceptive/mylibrarydemoapp/LogDemo.java
+++ mylibrarydemoapp/src/main/java/com/inceptive/mylibrarydemoapp/LogDemo.java
@@ -1,12 +1,19 @@
 package com.inceptive.mylibrarydemoapp;
 
+import static android.R.id.message;
+
 /**
  * Created by inceptive on 3/5/17.
  */
 
 public class LogDemo {
 
-    LogDemo(){
-        System.out.println("HelloWorld");
-    }
+
+
+      public static int sum(int a, int b){
+
+            int c = a+b;
+            return c;
+        }
+
 }
