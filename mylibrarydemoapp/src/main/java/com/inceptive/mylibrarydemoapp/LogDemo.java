353dae6c102ae5fcc1919b9edb04e4d32e7253a9
rohitpanhalkar <rohit.panhalkar@inceptiveconsulting.com>
2017-05-03 12:16:05 +0530

    Created simple demo Library

---
+++ mylibrarydemoapp/src/main/java/com/inceptive/mylibrarydemoapp/LogDemo.java
@@ -0,0 +1,12 @@
+package com.inceptive.mylibrarydemoapp;
+
+/**
+ * Created by inceptive on 3/5/17.
+ */
+
+public class LogDemo {
+
+    LogDemo(){
+        System.out.println("HelloWorld");
+    }
+}
